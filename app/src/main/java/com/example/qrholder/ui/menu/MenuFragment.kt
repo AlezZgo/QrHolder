8c2e4f7fd9147f7b0462c31ebc437e2d5d6ac0dc
Александр <alez.grante@gmail.com>
2022-10-16 18:24:59 +0300

    icon changed,fab buttons,fab buttons added,ui layouts menu,fragment and home,fragment,clear, colors

--- app/src/main/java/com/example/qrholder/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/example/qrholder/ui/menu/MenuFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.qrholder.BuildConfig
 import com.example.qrholder.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
@@ -21,6 +22,7 @@ class MenuFragment : Fragment() {
     ): View {
 
         binding = FragmentMenuBinding.inflate(inflater, container, false)
+        binding.tvAppVersion.text = BuildConfig.VERSION_NAME
         return binding.root
     }
 
