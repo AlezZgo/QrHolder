f9a1157957f3a2518cc7082134eaa0f5d794c4f1
Александр <alez.grante@gmail.com>
2022-10-16 22:37:27 +0300

    fab buttons added

--- app/src/main/java/com/example/qrholder/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/qrholder/ui/home/HomeViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.qrholder.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel()
