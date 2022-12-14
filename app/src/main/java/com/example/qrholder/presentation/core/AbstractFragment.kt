b02da9bc88b5dfbb257c282301d843d7973341f8
Александр <alez.grante@gmail.com>
2022-12-15 01:27:44 +0300

    constructor fix

--- app/src/main/java/com/example/qrholder/presentation/core/AbstractFragment.kt
+++ app/src/main/java/com/example/qrholder/presentation/core/AbstractFragment.kt
@@ -6,11 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import com.example.qrholder.core.ui.AbstractViewModel
 
 typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
 
-abstract class AbstractFragment<B : ViewBinding, V : AbstractViewModel>(
+abstract class AbstractFragment<B : ViewBinding>(
     private val inflate: Inflate<B>,
 ) : Fragment() {
 
--- app/src/main/java/com/example/qrholder/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/qrholder/presentation/home/HomeFragment.kt
@@ -1,24 +1,20 @@
 package com.example.qrholder.presentation.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.qrholder.databinding.FragmentHomeBinding
 import com.example.qrholder.presentation.core.AbstractFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : AbstractFragment<FragmentHomeBinding,HomeViewModel>(FragmentHomeBinding::inflate) {
+class HomeFragment : AbstractFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
 
     private val viewModel by viewModels<HomeViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.init(savedInstanceState==null)
+        viewModel.init(savedInstanceState == null)
     }
-
 
 }
