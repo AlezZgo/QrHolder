c5337e6a49c707ed71e34873f0846d60563d7dac
Александр <alez.grante@gmail.com>
2022-12-15 01:19:45 +0300

    Abstract Fragment

---
+++ app/src/main/java/com/example/qrholder/presentation/core/AbstractFragment.kt
@@ -0,0 +1,29 @@
+package com.example.qrholder.presentation.core
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.example.qrholder.core.ui.AbstractViewModel
+
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
+
+abstract class AbstractFragment<B : ViewBinding, V : AbstractViewModel>(
+    private val inflate: Inflate<B>,
+) : Fragment() {
+
+    private var _viewBinding: B? = null
+    protected val binding get() = checkNotNull(_viewBinding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _viewBinding = inflate.invoke(inflater, container, false)
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/example/qrholder/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/qrholder/presentation/home/HomeFragment.kt
@@ -7,28 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.qrholder.databinding.FragmentHomeBinding
+import com.example.qrholder.presentation.core.AbstractFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : AbstractFragment<FragmentHomeBinding,HomeViewModel>(FragmentHomeBinding::inflate) {
 
-    private lateinit var binding: FragmentHomeBinding
+    private val viewModel by viewModels<HomeViewModel>()
 
-    private val homeViewModel by viewModels<HomeViewModel>()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.init(savedInstanceState==null)
     }
 
-    override fun onResume() {
-        super.onResume()
-        homeViewModel.init(true)
-    }
 
 }
