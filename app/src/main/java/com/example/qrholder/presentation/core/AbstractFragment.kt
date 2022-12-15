20c7d36fa2776ea747359faa680b194ba535ae38
Александр <alez.grante@gmail.com>
2022-12-15 23:51:17 +0300

    Abstract fragment fix

--- app/src/main/java/com/example/qrholder/presentation/core/AbstractFragment.kt
+++ app/src/main/java/com/example/qrholder/presentation/core/AbstractFragment.kt
@@ -1,20 +1,36 @@
 package com.example.qrholder.presentation.core
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
+import com.example.qrholder.core.ui.AbstractViewModel
 
 typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
 
-abstract class AbstractFragment<B : ViewBinding>(
+abstract class AbstractFragment<B : ViewBinding, V : AbstractViewModel>(
     private val inflate: Inflate<B>,
+    private val clazz : Class<V>
 ) : Fragment() {
+
+    protected var useSharedViewModel: Boolean = false
+    lateinit var viewModel : V
 
     private var _viewBinding: B? = null
     protected val binding get() = checkNotNull(_viewBinding)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = if (useSharedViewModel) {
+            ViewModelProvider(requireActivity())[clazz]
+        } else {
+            ViewModelProvider(this)[clazz]
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +39,17 @@ abstract class AbstractFragment<B : ViewBinding>(
     ): View? {
         _viewBinding = inflate.invoke(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.init(savedInstanceState == null)
+        Log.d("tagger","Hello from onViewCreated Abstract")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _viewBinding = null
     }
 
 }
--- app/src/main/java/com/example/qrholder/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/qrholder/presentation/home/HomeFragment.kt
@@ -1,20 +1,14 @@
 package com.example.qrholder.presentation.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import androidx.fragment.app.viewModels
 import com.example.qrholder.databinding.FragmentHomeBinding
 import com.example.qrholder.presentation.core.AbstractFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : AbstractFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-
-    private val viewModel by viewModels<HomeViewModel>()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel.init(savedInstanceState == null)
-    }
-
-}
+class HomeFragment : AbstractFragment<FragmentHomeBinding, HomeViewModel>(
+    FragmentHomeBinding::inflate,
+    HomeViewModel::class.java
+)
