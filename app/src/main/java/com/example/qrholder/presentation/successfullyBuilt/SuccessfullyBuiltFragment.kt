49fffc722979eb452f238140d8218c13abcb8df3
Александр <alez.grante@gmail.com>
2023-01-19 22:51:19 +0300

    Successfully creation screen

--- app/src/main/java/com/example/qrholder/presentation/successfullyBuilt/SuccessfullyBuiltFragment.kt
+++ app/src/main/java/com/example/qrholder/presentation/successfullyBuilt/SuccessfullyBuiltFragment.kt
@@ -1,5 +1,6 @@
 package com.example.qrholder.presentation.successfullyBuilt
 
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.qrholder.databinding.FragmentSuccessfullyBuiltBinding
 import com.example.qrholder.presentation.core.fragment.AbstractFragment
@@ -15,6 +16,13 @@ class SuccessfullyBuiltFragment :
         args.qrCode.loadImage(binding.ivQrCode)
         args.qrCode.loadTitle(binding.tvTitle)
         args.qrCode.loadContent(binding.tvContent)
+    }
+
+    override fun setupListeners() {
+        super.setupListeners()
+        binding.BtnOk.setOnClickListener {
+            findNavController().navigate(SuccessfullyBuiltFragmentDirections.actionSuccessfullyBuiltFragmentToNavigationHome())
+        }
     }
 }
 
