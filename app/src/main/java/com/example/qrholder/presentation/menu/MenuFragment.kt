4cc6aac2b7d194f5926aad2041b9cc27e123bfb4
Александр <alez.grante@gmail.com>
2023-01-25 01:28:09 +0300

    fix tests

--- app/src/main/java/com/example/qrholder/presentation/menu/MenuFragment.kt
+++ app/src/main/java/com/example/qrholder/presentation/menu/MenuFragment.kt
@@ -1,30 +1,26 @@
 package com.example.qrholder.presentation.menu
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import com.example.qrholder.BuildConfig
+import android.content.Intent
+import android.net.Uri
 import com.example.qrholder.databinding.FragmentMenuBinding
+import com.example.qrholder.presentation.core.fragment.AbstractFragment
 import com.example.qrholder.presentation.core.fragment.BottomNavViewVisibility
 
-class MenuFragment : Fragment(), BottomNavViewVisibility.Show {
 
-    private lateinit var binding: FragmentMenuBinding
+class MenuFragment : AbstractFragment<FragmentMenuBinding,MenuViewModel>(
+    FragmentMenuBinding::inflate,MenuViewModel::class.java
+), BottomNavViewVisibility.Show {
 
-    private val menuViewModel by viewModels<MenuViewModel>()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-
-        binding = FragmentMenuBinding.inflate(inflater, container, false)
-        binding.tvAppVersion.text = BuildConfig.VERSION_NAME
-        return binding.root
+    override fun setupViews() {
+        super.setupViews()
+        binding.cvSupport.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW, Uri.parse(
+                        "https://t.me/AlezZGO"
+                    )
+                )
+            )
+        }
     }
-
 }
